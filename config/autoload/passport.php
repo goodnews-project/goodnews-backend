c0bf750cd578cedfca31b7098690b2f70bb364fe
hzmain <hzmain5@gmail.com>
2024-10-22 14:35:51 +0800

    change passport key path

--- config/autoload/passport.php
+++ config/autoload/passport.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use function Hyperf\Support\env;
 
 return [
-    'key_store_path'     => 'storage/keys',
+    'key_store_path'     => 'storage',
     'client_uuids'       => true,
     'key'                => 'CpmLVtjV8diGbhEsVD3IWoVOn31pRpmupEcxMCgtXp9LGpe39F',
     'token_days'         => null,
